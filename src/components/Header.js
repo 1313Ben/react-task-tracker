c784c98ee0bdf1ee8953a1dfaa4a0811e9c86814
Ravindra Benadict Asirvatham <ravindra_github@bluewin.ch>
2021-09-26 15:23:13 +0200

    added css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+
+const Header = (props) => {
   return (
     <header>
-      <h1>Task Tracker</h1>
+      <h1 style={headingStyle}>{props.title}</h1>
     </header>
   )
 }
 
+Header.defaultProps = {
+  title: 'Task Tacker default',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+// CSS in JS
+const headingStyle = {
+  // color: 'green',
+  // backgroundColor: 'black',
+}
 export default Header
