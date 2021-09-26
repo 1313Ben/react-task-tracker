a45620104b234ad0f5d6c77d98f0cbf319e9ee00
Ravindra Benadict Asirvatham <ravindra_github@bluewin.ch>
2021-09-26 19:47:18 +0200

    Added click logic to the button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,25 @@
-const Button = () => {
+import PropTypes from 'prop-types'
+
+const Button = ({ text,color,onClick }) => {
   return (
-      <button className='btn'>Add</button>
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className='btn'
+    >
+      {text}
+    </button>
   )
+}
+
+Button.defaultProps = {
+  color: 'steelblue',
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,17 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('click')
+  }
+
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button />
+      <Button
+        text="Add"
+        color='green'
+        onClick={onClick}/>
     </header>
   )
 }
